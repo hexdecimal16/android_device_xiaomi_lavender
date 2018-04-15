e8a535907693f9600d3adc5414a30d556228e48c
Demon Singur <demonsingur@gmail.com>
2018-04-15 10:55:05 +0000

    lavender: Import light HAL
    Change-Id: I4fa56abd38c04d8fa267dbab67e54fe4a617bfe1

--- device.mk
+++ device.mk
@@ -106,6 +106,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.xiaomi_sdm660
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     lineage.livedisplay@2.0-service-sdm

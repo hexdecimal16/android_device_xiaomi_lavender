1b78fa73fee12bffeb5a780559541c9c5903a6ee
Hexdecimal16 <Hexdecimal16@gmail.com>
2019-05-15 03:16:58 -0400

    lavender: added IPv6 packages

--- device.mk
+++ device.mk
@@ -226,6 +226,11 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# IPv6
+PRODUCT_PACKAGES += \
+    ebtables \
+    ethertypes
+
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \

63ca0fea5dba23b10da208d2dd1c2d620c1ab150
Hexdecimal16 <Hexdecimal16@gmail.com>
2019-05-22 22:48:24 +0530

    lavender: low audio on voice call fix

--- system-props.mk
+++ system-props.mk
@@ -1,7 +1,8 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.media_vol_steps=25 \
-    ro.config.vc_call_vol_steps=7
+    ro.config.vc_call_vol_steps=7 \
+    ro.vendor.audio.sdk.fluencetype=fluence
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
